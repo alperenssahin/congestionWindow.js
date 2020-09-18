e849e8d53ec9c8a8ad8bcc33663da2ca416dd7a5
Alperen Şahin <allperensahin@gmail.com>
2020-09-18 21:35:08 +0300

    module.export

--- congestionWindow.js
+++ congestionWindow.js
@@ -1,24 +1,21 @@
-(async ()=>{
-    async function foo(){
-        return new Promise((resolve => {
-            setTimeout(()=>{resolve()},200+800*Math.random());
-        }))
-    }
+module.exports = async (dataArray,loadFunction= async()=>{}) =>{
+    let promiseArray = []
     let congestionWindowSize = 10;
-    let congestionTimeout = 900;
-    let promiseArray = [];
-    for(let i=0; i<1000; i++){
-        promiseArray.push(foo());
-        if(promiseArray.length >= congestionWindowSize){
-            console.log(`${promiseArray.length} promises in progress`);
-            let congestionTimeoutHandler = setTimeout(()=>{
-                console.log("CONGESTION TIMEOUT");
+    let congestionTimeout = 2000;
+    for(let data of dataArray){
+        promiseArray.push(loadFunction(data))
+        if(congestionWindowSize < 1) congestionWindowSize = 1;
+        if(promiseArray.length>=congestionWindowSize){
+            let congestionWindowHandler = setTimeout(()=>{
+                // console.log("Congestion W" , congestionWindowSize);
                 congestionWindowSize /= 2;
             },congestionTimeout);
             await Promise.all(promiseArray);
-            clearTimeout(congestionTimeoutHandler);
+            console.log("CongestionWindow Size",congestionWindowSize);
+            console.log("Progress", `${dataArray.indexOf(data)}/${dataArray.length}`);
             promiseArray = [];
+            clearTimeout(congestionWindowHandler);
             congestionWindowSize++;
         }
     }
-})();
+}
